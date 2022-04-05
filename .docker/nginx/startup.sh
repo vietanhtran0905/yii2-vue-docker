cedee95d9e9b1f4be5c9b64228a54c120d525250
Tran Viet Anh <anhtv@MacBook-Pro-cua-Tran.local>
2022-04-05 12:22:48 +0700

    config vue

--- .docker/nginx/startup.sh
+++ .docker/nginx/startup.sh
@@ -4,5 +4,5 @@
 
 chown nginx:nginx /var/run/php/php8.1-fpm.sock
 
-
-cd /var/www/html/frontend && pm2 start "npm run serve"
+cd /var/www/html/frontend && pm2 start "yarn serve"
+wait $!
--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
+const path = require("path");
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  outputDir: path.resolve(__dirname, "../api/api/web/frontend"),
+  publicPath: '/frontend'
 })
